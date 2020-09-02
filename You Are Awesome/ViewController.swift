0731d99b7fc5e1ba0e5b9fe53839b740e3d31c16
Alex McCullough <amccullough2@zagmail.gonzaga.edu>
2020-09-02 11:44:42 -0700

    Initial Commit

---
+++ You Are Awesome/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  You Are Awesome
+//
+//  Created by Alexander McCullough on 9/2/20.
+//  Copyright © 2020 Alexander McCullough. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
